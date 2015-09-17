6ede7335917398c54b35b78334b858fceb1af4bf
Jonathan Sommer <jonathan.sommer@macio.de>
2015-09-17 13:50:04 +0200

    change plugin xml file

--- src/android/Practise.java
+++ src/android/Practise.java
@@ -27,6 +27,6 @@ import org.json.JSONException;
 public class Practise extends CordovaPlugin {
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		
+		return true;
 	}
 }
