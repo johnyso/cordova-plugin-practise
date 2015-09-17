07fa2b10500d6d6d0b94a89a98677fa80b3363f3
Jonathan Sommer <jonathan.sommer@macio.de>
2015-09-17 14:21:36 +0200

    change plugin xml file

--- src/android/Practise.java
+++ src/android/Practise.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package com.example.myplugin;
+package com.example.cordova.practise;
  
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
